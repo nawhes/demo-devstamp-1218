d76230dab354682b9a118381d4639ee04be65888
lastmagic <youheedong@gmail.com>
2018-12-18 15:17:53 +0900

    VIB-000 Add demo solidity file

---
+++ demoHomelessDog.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.18;
+contract homelessDogs {
+  string [8] owner;
+  function setOwner(uint _index, string _name) public {
+    owner[_index] = _name;
+  }
+
+  function getOwner(uint _index) public view returns (string) {
+    return owner[_index];
+  }
+}
